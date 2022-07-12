576c5a3842403e137446696e1b33359879a961e5
Kishlaya1999 <kishlaya54321@gmail.com>
2022-07-12 19:03:05 +0530

    Added a view in view folder named users.ejs and user_controller is rendering it

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,7 @@
 module.exports.profile = function(req,res){
-     return res.end("<h1>User Profile</h1>")
+     // return res.end("<h1>User Profile</h1>")
+     return res.render('users',{
+          title : "Users"
+     })
 }
 
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const router =express.Router();
 const usersController = require('../controllers/users_controller');
 const postsController = require('../controllers/posts_controller');
 
+
 router.get('/profile',usersController.profile);
 router.get('/posts',postsController.posts)
 
