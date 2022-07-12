27d7c8b3ba225c4524f5b28964007a11bbbd0617
Kishlaya1999 <kishlaya54321@gmail.com>
2022-07-12 18:51:17 +0530

    Added a view in view folder named home.ejs and home_controller is rendering it

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,12 @@
 
 // Actions imported by routes/index.js
 module.exports.home = function(req, res){
-     return res.end("<h1>Express is up for Codial</h1>");
+     // return res.end("<h1>Express is up for Codial</h1>");
+     return res.render('home',{
+          title: "Home"
+     })
 }
+
+// Syntax
+//module.exports.actionName = function(req,res){work}
 
