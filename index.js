f372f2a0f3ce5cc584ae3bdbeaaabc1d823a4e75
Kishlaya1999 <kishlaya54321@gmail.com>
2022-07-12 18:44:50 +0530

    setting up the view engine i.e ejs

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = 8000;
 // Middleware to acess the routes/index.js by default index.js is loaded so we can write ./routes only
 app.use('/',require('./routes'));
 
+// set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port, function(err){
      if(err){
           console.log(`Error in running the server: ${err}`);
