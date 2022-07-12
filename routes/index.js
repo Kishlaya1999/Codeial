b2b7814f8bfe479184f286650488d11355e7bafb
Kishlaya1999 <kishlaya54321@gmail.com>
2022-07-12 17:01:21 +0530

    Controller created

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 // Everytime we require express it is not going to create the new instance of express it is going to fetch the new instance
 const express = require('express');
 
-
 const router = express.Router();
+// impporting the controller file so that we can specify it with the routes
+const homeController = require('../controllers/home_controller');
+
+// Specifing the routes 
+
+// Action for ./ route is defined in controller/homeController 
+router.get('./',homeController.home)
+
 
 
 // exporting it so that it is avalilable to index.js
