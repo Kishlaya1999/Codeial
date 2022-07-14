7f6f6f5e04b76aa5fd1489561375a7913a56e2b1
Kishlaya1999 <kishlaya54321@gmail.com>
2022-07-14 12:28:02 +0530

    MongoDB server setup and user schema created

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
 // telling the middleware that where to look for assets
 app.use(express.static('./assets'));
---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+     email:{
+          type: String,
+          required: true,
+          unique: true
+     },
+     password:{
+          type: String,
+          required: true,
+     },
+     name:{
+          type:String,
+          required: true
+     }
+},{
+     // Contains the time of creation and updation of object as createdAt and updatedAt
+     timestamps: true
+});
+
+const user = mongoose.model('User',userSchema);
+
+module.exports = User;
