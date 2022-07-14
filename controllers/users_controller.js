511540c828c7cf333a2377d73850052abe1edf3d
Kishlaya1999 <kishlaya54321@gmail.com>
2022-07-14 13:36:33 +0530

    add user-sign-up & user-sign-in-page and setup there routes and controllers

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,3 +5,26 @@ module.exports.profile = function(req,res){
      })
 }
 
+// Render the sign up page
+module.exports.signUp = function(req,res){
+     return res.render('user_sign_up',{
+          title: 'Codeial | Sign Up'
+     })
+}
+
+// Render the sign in page
+module.exports.signIn = function(req,res){
+     return res.render('user_sign_in',{
+          title: 'Codeial | Sign In'
+     })
+}
+
+// get the sign up data
+module.exports.create = function(req,res){
+     // TODO
+}
+
+// sign in and create the sission for the user
+module.exports.createSession = function(req, res){
+     // TODO
+}
--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,9 @@ const postsController = require('../controllers/posts_controller');
 
 
 router.get('/profile',usersController.profile);
-router.get('/posts',postsController.posts)
+router.get('/posts',postsController.posts);
+router.get('/signUp',usersController.signUp)
+router.get('/signIn',usersController.signIn)
+
 
 module.exports = router;
