4dd7279a09ecaf3a39929f7d128980a8aacb849c
Kishlaya1999 <kishlaya54321@gmail.com>
2022-07-17 10:44:13 +0530

    Installed passport and passport-local and setup passport

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,50 @@
+const passport = require('passport');
+
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('../models/user');
+
+
+// Authentication using passport
+passport.use(new LocalStrategy({
+     usernameField: 'email',
+     },
+     function(email, password, done){
+          // find a user and establish the identity
+          User.findOne({email:email}, function(err, user){
+               if(err){
+                    console.log('Error in finding user --> passport');
+                    return done(err);
+               }
+               
+               if(!user || user.password != password){
+                    console.log('Invalid username/password');
+                    return done(null, false);
+               }
+
+               return done(null, user);
+          })
+     }
+));
+
+
+// Serializing the user to decide which key is to be kept in the cookies
+passport.serializeUser(function(user, done){
+     done(null, user.id)
+});
+
+
+// deserializing the user from the key in the cookies
+passport.deserializeUser(function(id, done){
+     User.findById(id, function(err, user){
+          if(err){
+               console.log('Error in finding user --> passport');
+               return done(err);
+          }
+
+          return done(null, user)
+     })
+});
+
+
+module.exports = passport;
