2f0e36116da1f27661ee35f9915fbd462a4d5eb9
Kishlaya1999 <kishlaya54321@gmail.com>
2022-07-18 11:24:47 +0530

    styled header and footer using scss

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const MongoStore = require('connect-mongo');
 const sassMiddleware = require('node-sass-middleware');
 
 app.use(sassMiddleware({
-     src: '/assets/scss',
-     dest: '/assets/css',
+     src: './assets/scss',
+     dest: './assets/css',
      debug: true, 
      outputStyle: 'extended',
      prefix: '/css'
